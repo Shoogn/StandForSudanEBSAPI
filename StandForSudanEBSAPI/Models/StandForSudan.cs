3dc511055518b1fde02ee6139785feeb831cb585
Mohammed <Mohammed.Hussien@outlook.sa>
2020-04-05 20:46:05 +0300

    Added [JsonProperty("NumberOfTransaction")] attribute to StandForSudan class

--- StandForSudanEBSAPI/Models/StandForSudan.cs
+++ StandForSudanEBSAPI/Models/StandForSudan.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace StandForSudanEBSAPI.Models
     public class StandForSudan
     {
         public StandForSudan() { }
+        [JsonProperty("NumberOfTransaction")]
         public long No_OfTransaction { get; set; }
         public double TotalAmount { get; set; }
     }
